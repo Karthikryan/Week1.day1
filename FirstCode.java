05a370308e81303fb9b2979a01838c83505e7214
Karthikryan <109000014+Karthikryan@users.noreply.github.com>
2022-07-09 21:41:51 +0100

    Update FirstCode.java

--- FirstCode.java
+++ FirstCode.java
@@ -3,8 +3,7 @@ package week1.day1;
 public class FirstCode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Welcome to TestLeaf");
+	System.out.println("Welcome to TestLeaf");
 	}
 
 }
