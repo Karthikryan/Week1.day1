dac1227e24d7714ccb7f959ca7abe7ba4bb6f551
Karthikryan <109000014+Karthikryan@users.noreply.github.com>
2022-07-09 21:40:52 +0100

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 15;
+		
+		if (i%2==1) {
+			
+			System.out.println("odd");
+		}
+		
+			else 
+			{
+				System.out.println("even");
+			
+		}
+
+	
+	}
+}
